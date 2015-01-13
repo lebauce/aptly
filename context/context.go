b8b4f251287bd40f47a4b1f86f36336e735b91fd
Sylvain Baubeau <sbaubeau@redhat.com>
2015-01-13 11:23:06 +0100

    Fix wrong argument order

--- context/context.go
+++ context/context.go
@@ -272,7 +272,7 @@ func (context *AptlyContext) GetPublishedStorage(name string) aptly.PublishedSto
 
 			var err error
 			publishedStorage, err = swift.NewPublishedStorage(params.UserName, params.Password,
-				params.AuthUrl, params.Tenant, params.TenantId, params.Prefix, params.Container)
+				params.AuthUrl, params.Tenant, params.TenantId, params.Container, params.Prefix)
 			if err != nil {
 				Fatal(err)
 			}
